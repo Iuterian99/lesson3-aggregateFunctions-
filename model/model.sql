c6891d74e1428660e072bffd260b3dd2c19b96b7
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-17 18:09:07 +0500

    connected vs express

---
+++ model/model.sql
@@ -0,0 +1,21 @@
+-- we are in database called "basics";
+
+CREATE TABLE courses(
+  course_id serial PRIMARY KEY,
+  course_name varchar(30) not null,
+  course_price = int not null,
+);
+
+CREATE TABLE groups(
+  group_id serial PRIMARY KEY,
+  group_name varchar(30) not null,
+  group_teacher varchar(30) not null,
+  group_of int REFERENCES courses(course_id)
+);
+
+CREATE TABLE students(
+  student_id serial PRIMARY KEY,
+  student_name varchar(30) not null,
+  student_of int REFERENCES groups(group_id)
+);
+
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const { Pool } = require('pg')
 
-
-app.get("/", (req, res) =>{
-  res.send("ok")
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'abdu1882',
+  database: 'basics',
+})
+app.get('/', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const { rows } = await client.query('SELECT * FROM  restaurants')
+    console.log(rows)
+    client.release()
+    res.send('ok')
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.listen(9000, console.log(9000))
